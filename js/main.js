3786e7d7d113eae37d561542329d5a00ceafbf52
Alexander Moreno <alexander.morenoc@outlook.com>
2021-01-07 15:31:35 -0500

    anio y mes generado automaticamente

--- js/main.js
+++ js/main.js
@@ -7,7 +7,26 @@ $tarjeta.addEventListener('click', ()=>{
 
 
 const btnOpenForm = document.querySelector('#btn-open-form')
+const formulario= document.querySelector('#form-card')
 
 btnOpenForm.addEventListener('click', () => {
     btnOpenForm.classList.toggle('active')
+    formulario.classList.toggle('active')
 })
+
+//*Select del mes generado dinamicamente
+for(let i=1; i<=12; i++){
+    let opcion=document.createElement('option')
+    opcion.value=i
+    opcion.innerText=i
+    formulario.selectMonth.appendChild(opcion)
+}
+
+//* Select del año generado dinamicamente
+const yearActual= new Date().getFullYear()
+for(let i=yearActual; i<= yearActual +8; i++){
+    let opcion=document.createElement('option')
+    opcion.value=i;
+    opcion.innerText=i;
+    formulario.selectYear.appendChild(opcion)
+}
